a5cc3a9b5e31aac18491a0f4da9f5f8c712a29ca
Dwight Spencer <dspencer@computekindustries.com>
2021-03-15 02:50:45 -0500

    (bugfix) resolves #1

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
 variable "AWS_ACCOUNT_ID" {
-	type   = string
+  type = string
 }
 
 variable "AWS_DEFAULT_REGION" {
-	default = "us-east-1"
-	type    = string
+  default = "us-east-1"
+  type    = string
 }
 
 variable "instant_name" {
-	default = "express_api"
-	type    = string
+  default = "express_api"
+  type    = string
 }
 
 ## Our image name built by cicd
-variable  "image_name" {
-	default = "bitnami/express"
-	type    = string
+variable "image_name" {
+  default = "bitnami/express"
+  type    = string
 }
 
 provider "aws" {
-	region = var.AWS_DEFAULT_REGION
+  region = var.AWS_DEFAULT_REGION
 }
 
 ## TODO: Fix container definition, use a template or better a docker-stack.yml
 ## https://github.com/rearc/terraform-aws-ecs-task/blob/857d3f8cc36aa92d2b074626b3fc69d2f6b9de3e/main.tf#L11
 
 module "ecs_task" {
-  source         = "git::https://github.com/rearc/terraform-aws-ecs-task.git"
+  source         = "./infrastructure"
   aws_account_id = var.AWS_ACCOUNT_ID
   region         = var.AWS_DEFAULT_REGION
   name           = var.instant_name

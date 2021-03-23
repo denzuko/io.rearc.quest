fd92991709555b6d2769ece06b34b63e784d96ff
Dwight Spencer <dspencer@computekindustries.com>
2021-03-23 01:23:46 -0500

    (feat) support bogie style deployments

--- tasks.py
+++ tasks.py
@@ -5,7 +5,7 @@ from os import remove
 from os.path import basename, dirname, realpath, exists, isdir, isfile
 from shutil import rmtree
 #from dotenv import load_dotenv
-from invoke import task
+from invoke import task, Collection, call
 
 #def load_env(path=f'{PWD}/.env'):
 #    return load_dotenv(dotenv_path=path)
@@ -13,29 +13,44 @@ from invoke import task
 def pwd():
     return dirname(realpath(__file__))
 
-@task(default=True)
-def docker_image(ctx):
-    tag = basename(pwd())
+@task
+def build_image(ctx, tag='ghcr.io/denzuko/clients/io.rearc.quest:latest'):
 #    env = load_env()
-    ctx.run(f"docker image build -t {tag}:latest .")
+    ctx.run(f"docker image build -t {tag} .")
+    ctx.run(f"docker push {tag}")
 
 @task
-def tf_init(ctx):
+def init(ctx):
     #env = load_env()
     ctx.run("terraform init")
 
-@task(pre=[tf_init])
-def tf_plan(ctx):
+@task(pre=[init])
+def plan(ctx, destroy=False):
     #env = load_env()
-    ctx.run("terraform plan")
-
-@task(pre=[docker_image,tf_plan])
-def deploy(ctx, tag='ghcr.io/denzuko/clients/io.rearc.quest:latest'):
-    #env = load_env()
-    ctx.run(f"docker push ${tag}")
-    ctx.run("terraform apply")
+    ctx.run(" ".join([
+        "terraform plan -out=plan.tfplan",
+        "-destroy" if destroy else ""
+        ]))
 
 @task
+def fmt(ctx):
+#    env = load_env()
+    ctx.run("terraform fmt -diff -recursive")
+
+@task(pre=[init, fmt])
+def validate(ctx):
+#    env = load_env()
+    ctx.run("terraform validate .")
+
+@task(pre=[plan])
+def apply(ctx):
+    ctx.run("terraform apply plan.tfplan")
+
+@task(pre=[call(plan, destroy=True)], post=[apply])
+def destroy(ctx):
+    pass
+
+@task(pre=[destroy])
 def clean(ctx):
     for x in [
         ".terraform",
@@ -48,3 +63,17 @@ def clean(ctx):
 
         if (exists(x) and isfile(x)):
             remove(x)
+
+@task(pre=[build_image,apply])
+def deploy(ctx):
+    pass
+
+@task(default=True)
+def help(ctx):
+    ctx.run('invoke --list')
+
+ns = Collection(clean, deploy, help)
+ns.add_collection(Collection('docker', build_image))
+ns.add_collection(Collection('terraform', 
+    init, plan, fmt, 
+    validate, apply, destroy))
